1dfc12a9fd1611756608f02b4024ecbd81832dd7
vivek <vivek.koyani@gmail.com>
2019-03-08 16:07:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView programmingList=(RecyclerView) findViewById(R.id.programmingList);
+        programmingList.setLayoutManager(new LinearLayoutManager(this));
+        String[] languages = {"Java", "JavaScript","C#","Php","c","c++","Python"};
+        programmingList.setAdapter(new ProgrammingAdapter(languages));
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/ProgrammingAdapter.java
@@ -0,0 +1,47 @@
+package com.example.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProgrammingAdapter extends RecyclerView.Adapter<ProgrammingAdapter.ProgrammingViewHolder> {
+
+    private String[] data;
+    public ProgrammingAdapter(String[] data)
+    {
+        this.data=data;
+    }
+
+    @NonNull
+    @Override
+    public ProgrammingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view= inflater.inflate(R.layout.list_item_layout,parent,false);
+        return new ProgrammingViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProgrammingViewHolder holder, int position) {
+        String title = data[position];
+        holder.txtTitle.setText(title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class ProgrammingViewHolder extends RecyclerView.ViewHolder{
+        ImageView imgIcon;
+        TextView txtTitle;
+        public ProgrammingViewHolder( View itemView) {
+            super(itemView);
+            imgIcon = (ImageView)itemView.findViewById(R.id.imageIcon);
+            txtTitle = (TextView)itemView.findViewById(R.id.txtTitle);
+        }
+    }
+}
